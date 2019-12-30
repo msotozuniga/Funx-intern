bf279caccc620f74ccb20e3d736276eb05ec9abd
Matías Soto <matias.soto.z@ug.uchile.cl>
2019-12-30 04:16:02 -0300

    Modify old database

--- syncMatch.py
+++ syncMatch.py
@@ -14,7 +14,7 @@ from synchronize.models import *
 def updateDatabase(response):
     delete_keys = ['slug', 'period', 'timestamp', 'datetime', 'championship', 'referee', 'stadium',
                    'local', 'away', 'local_goals', 'local_penalty_goals', 'away_goals', 'away_penalty_goals',
-                   'scorer', 'state','actions']
+                   'scorer', 'state', 'actions']
     list = json.loads(response)
     for i in range(len(list)):
         m, create = Match.objects.get_or_create(pk=i, defaults={
@@ -23,15 +23,18 @@ def updateDatabase(response):
             'duration': list[i]['timestamp'],
             'match_date': datetimeParser(list[i]['datetime'])
         })
+        championship = list[i]['championship']
+        referee = list[i]['referee']
+        stadium = list[i]['stadium']
+        local = list[i]['local']
+        away = list[i]['away']
+        estate = list[i]['state']
         if create:
-            championship = list[i]['championship']
             c, dummy = Championship.objects.get_or_create(slug=championship['slug'],
                                                           defaults={'name': championship['name']})
             m.championship = c
-            referee = list[i]['referee']
             r, dummy = Referee.objects.get_or_create(name=referee['name'])
             m.referee = r
-            stadium = list[i]['stadium']
             s, dummy = Stadium.objects.get_or_create(latitude=stadium['latitude'],
                                                      longitude=stadium['longitude'],
                                                      defaults={
@@ -41,14 +44,13 @@ def updateDatabase(response):
             m.stadium = s
             m.save()
 
-            local = list[i]['local']
             l, dummy = Team.objects.get_or_create(slug=local['slug'],
                                                   defaults={
                                                       'name': local['name'],
                                                       'short_name': local['short_name'],
                                                       'image': local['image']
                                                   })
-            away = list[i]['away']
+
             a, dummy = Team.objects.get_or_create(slug=away['slug'],
                                                   defaults={
                                                       'name': away['name'],
@@ -63,11 +65,10 @@ def updateDatabase(response):
                                          local_penalty=list[i]['local_penalty_goals'],
                                          away_goals=list[i]['away_goals'],
                                          away_penalty=list[i]['away_penalty_goals'])
-            e = list[i]['state']
 
             state = State.objects.create(match=m,
-                                         type=e['type'],
-                                         name=e['name'])
+                                         type=estate['type'],
+                                         name=estate['name'])
 
             for each in list[i]['actions']:
                 Action.objects.create(match=m,
@@ -79,11 +80,72 @@ def updateDatabase(response):
             for key in list[i]:
                 if type(list[i][key]) is dict:
                     for each in list[i][key]:
-                        MatchData.objects.create(match=m,characteristics=each,status=list[i][key][each])
+                        MatchData.objects.create(match=m, characteristics=each, status=list[i][key][each])
                 else:
                     MatchData.objects.create(match=m, characteristics=key, status=list[i][key])
+        else:
+            m.slug = list[i]['slug']
+            m.period = list[i]['period']
+            m.duration = list[i]['timestamp']
+            m.match_date = datetimeParser(list[i]['datetime'])
+            m.save()
+            c = m.championship
+            c.name = championship['name']
+            c.slug = championship['slug']
+            c.save()
+            r = m.referee
+            r.name = referee['name']
+            r.save()
+            s = m.stadium
+            s.name = stadium['name']
+            s.slug = stadium['slug']
+            s.city = stadium['city']
+            s.latitude = stadium['latitude']
+            s.longitude = stadium['longitude']
+            oldscore = Score.objects.get(match=m)
+            l = oldscore.local
+            l.slug = local['slug']
+            l.name = local['name']
+            l.short_name = local['short_name']
+            l.image = local['image']
+            l.save()
+            a = oldscore.away
+            a.slug = away['slug']
+            a.name = away['name']
+            a.short_name = away['short_name']
+            a.image = away['image']
+            a.save()
+            oldscore.local_goals = list[i]['local_goals']
+            oldscore.local_penalty = list[i]['local_penalty_goals']
+            oldscore.away_goals = list[i]['away_goals']
+            oldscore.away_penalty = list[i]['away_penalty_goals']
+            oldscore.score_s = list[i]['scorer']
+            oldscore.save()
+            oldstate = State.objects.get(match=m)
+            oldstate.name = estate['name']
+            oldstate.type = estate['type']
+            oldstate.save()
+            actions_list = Action.objects.filter(match=m)
+            for j in range(len(actions_list)):
+                actions_list[j].type = list[i]['actions'][j]['type']
+                actions_list[j].icon = list[i]['actions'][j]['icon']
+                actions_list[j].url = list[i]['actions'][j]['url']
+                actions_list[j].save()
+            entries_to_remove(delete_keys, list[i])
+            for key in list[i]:
+                if type(list[i][key]) is dict:
+                    for each in list[i][key]:
+                        d, fabricate = MatchData.objects.get_or_create(match=m, characteristics=each,
+                                                                       defaults={'status': list[i][key][each]})
+                        if not fabricate:
+                            d.status = list[i][key][each]
+                else:
+                    d, fabricate = MatchData.objects.get_or_create(match=m, characteristics=key,
+                                                                   defaults={'status': list[i][key]})
+                    if not fabricate:
+                        d.status = list[i][key]
 
-        pass
+    return
 
 
 cache = ''
